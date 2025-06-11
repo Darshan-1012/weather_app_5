c31995f5bc51ebec7dc558e497432278d5ff3cb6
Darshan-1012 <bdarshan524@gmail.com>
2025-06-11 19:02:12 +0530

    Widget lifecycle --> initState & setState in Stateful widgets

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,8 +8,17 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
+  int counter =0;
+
+  @override
+  void initState() {
+    super.initState();
+    print('initState function raan');
+  }
   @override
   Widget build(BuildContext context) {
+    print('build State function raan');
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
@@ -18,7 +27,14 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: Text('Choose location screen'),
+      body: ElevatedButton(
+        onPressed: (){
+          setState((){
+          counter+=1;
+        });
+        },
+        child: Text('counter is $counter'),
+        ),
     );
   }
 }
